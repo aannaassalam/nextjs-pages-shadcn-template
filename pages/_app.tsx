5e8d22a1c0a38f97d2404751e4f0ba328d6f6b20
Anas Alam <anasalam027@gmail.com>
2025-08-30 13:31:16 +0530

    Template initialized

---
+++ pages/_app.tsx
@@ -0,0 +1,94 @@
+import EventListeners from "@/components/EventListener/EventListener";
+import { checkWindow } from "@/lib/functions/_helpers.lib";
+import "@/styles/globals.css";
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+  QueryKey
+} from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import type { AppContext, AppProps } from "next/app";
+import App from "next/app";
+import React from "react";
+import { toast, Toaster } from "sonner";
+
+interface ErrorData {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+}
+
+/**
+ * It suppresses the useLayoutEffect warning when running in SSR mode
+ */
+function fixSSRLayout() {
+  // suppress useLayoutEffect (and its warnings) when not running in a browser
+  // hence when running in SSR mode
+  if (!checkWindow()) {
+    React.useLayoutEffect = () => {
+      // console.log("layout effect")
+    };
+  }
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      retry: 0
+    }
+  },
+  mutationCache: new MutationCache({
+    onSuccess: (data, _variables, _context, mutation) => {
+      const message = (data as AxiosResponse).headers["x-message"];
+      const showToast = mutation.meta?.showToast !== false;
+      if (showToast && message) {
+        toast.success(message);
+      }
+    },
+    onError: (res) => {
+      const result = res as unknown as ErrorData;
+      if (result?.response?.data?.message) {
+        toast.error(result?.response?.data?.message);
+      } else {
+        toast.error("An error occurred while processing your request.");
+      }
+    },
+    onSettled: (_data, _error, _variables, _context, mutation) => {
+      if (mutation?.meta?.invalidateQueries) {
+        queryClient.invalidateQueries({
+          queryKey: mutation?.meta?.invalidateQueries as QueryKey,
+          refetchType: "all"
+        });
+      }
+    }
+  })
+});
+
+export default function CustomApp({ Component, pageProps }: AppProps) {
+  fixSSRLayout();
+
+  return (
+    <main>
+      {/* <SessionProvider session={pageProps.session}> */}
+      <QueryClientProvider client={queryClient}>
+        <EventListeners />
+        <Toaster richColors position="bottom-left" />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+      {/* </SessionProvider> */}
+    </main>
+  );
+}
+
+CustomApp.getInitialProps = async (context: AppContext) => {
+  // // const client = initializeApollo({ headers: context.ctx.req?.headers });
+  // // resetServerContext();
+  const appProps = await App.getInitialProps(context);
+
+  return { ...appProps };
+};
